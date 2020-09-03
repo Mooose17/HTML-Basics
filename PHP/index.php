bee1cd1189a7da179dd53571aed6ef077691a1c0
Moose17 <63780114+Mooose17@users.noreply.github.com>
2020-09-03 21:20:34 +0100

    created my first php script!

---
+++ PHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index.php</title>
+</head>
+<body>
+
+<?php
+echo "My name is";
+$name = "Tommy";
+Echo "<h1>$name</h1>";
+?>
+
+</body>
+</html>
+
