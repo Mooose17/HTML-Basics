55939732b42510ab4002be27e1b7ef58da115aa0
Moose17 <63780114+Mooose17@users.noreply.github.com>
2020-09-04 19:57:17 +0100

    learned basic variable meaning and started numbers

---
+++ PHP/15_variables.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h1>Variables</h1>";
+
+$name = "Tommy";
+echo "Hello, $name!!!!";
+echo "<br>";
+echo "My name is Wizard.<br>";
+echo " $name, thats a great name, nice to meet you!!";
+# research super global variables
+?>
+
+<?php
+
+echo "<h2>Variable learned</h2>";
+
+$name = "Tommy";
+$age = "20";
+$gender = "male";
+$sexuality = "gay";
+
+echo "Hello, $name, you are $age years old and you are a $gender.
+<br> You have also told me you are $sexuality. "
+
+?>
