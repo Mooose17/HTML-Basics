c79e6d7ca91c135ca0e59f876341cede8ee6d9f0
Moose17 <63780114+Mooose17@users.noreply.github.com>
2020-09-05 13:21:25 +0100

    learned print_r and var_dump functions

---
+++ PHP/19_debugging_variables.php
@@ -0,0 +1,25 @@
+<?php echo "<h1>print_r() & var_dump()</h1>"?>
+
+<?php
+$family = [
+"John", "Sophie", "Danny", "Evie"
+];
+?>
+
+<?php 
+    echo $family[3];
+?>
+
+
+<?php
+$food = [
+    "hotdogs", "burgers", "fries", "nachoes"
+    ];
+?>
+
+<?php
+   var_dump($food);
+?>
+
+
+
