4df6f8f382dc33a7cfde653e5c6082c9a429f1db
Moose17 <63780114+Mooose17@users.noreply.github.com>
2020-09-05 12:50:02 +0100

    LEarned 5 data types

---
+++ PHP/16_data_types.php
@@ -0,0 +1,22 @@
+<?php echo "<h1>Data Types</h1>";?>
+
+<?php
+    $string = "My name is tommy";
+    //"__" words or letters in quotations are called strings
+    $int = 20;
+    //integer or "int" for short (no quotations)
+    $float = 3.14; 
+    // float (Any decimal number)
+    $boolean = true;
+    // true/false statements (Binary)
+    $array = ["one", "two", "three"];
+    /* array can have any value
+    i.e $array = ["one",$boolean,"$int", 3.14]; */
+
+#echo section
+
+    echo $string . "<br/>";
+    echo $int . "<br/>";
+    echo $float . "<br/>";
+    echo $boolean . "<br/>";
+    ?>
