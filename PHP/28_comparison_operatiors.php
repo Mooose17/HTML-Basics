fa9113d9f1d14bafb251da45b2803fff9d6d3972
Moose17 <63780114+Mooose17@users.noreply.github.com>
2020-09-21 19:38:13 +0100

    learned strict comparison ===

---
+++ PHP/28_comparison_operatiors.php
@@ -0,0 +1,47 @@
+<?php echo "<h2>Comparison Operators</h2>"; ?>
+If Harrison Ford was a Programmer...<br />
+We'd call him.... Comparison Ford. ¯\_(ツ)_/¯
+<hr />
+
+<?php 
+/*
+	$age = 20;
+
+	// == is equal to 
+	// != is not equal to
+	if($age != 18) {
+		echo "You are NOT 18!";
+	} else {
+		echo "You are 18!!!!!";
+	}
+
+	$name = "Ironman";
+
+	if($name != "Kalob") {
+		echo "You are not the teacher!!";
+	}
+
+	echo "<hr />";
+
+	// Data types are important.
+
+
+	$books = 3;
+
+	if($books === "3") {
+		echo "You have three books (string)";
+	} elseif ($books === 3) {
+		echo "Books is three (int)";
+	} else {
+		echo "Something else";
+	}
+*/
+
+$age = 21;
+    if($age<=20){
+        echo "You are 20 years old";
+    }elseif($age===21){
+        echo "You can drink in America!! VAGAS BABY!!";
+    }else{
+        echo "You are alive?";
+    }
